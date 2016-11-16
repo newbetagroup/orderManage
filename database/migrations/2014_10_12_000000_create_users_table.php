0f7fd33cbd6e41eeb851380589cfd41f59a6dc89
geek-zwb <515639342@qq.com>
2016-11-16 18:01:53 +0800

    创建用户表

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,17 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('password', 255);
+            $table->text('avatar_url')->nullable()->comment('头像');
+            $table->string('domicile',255)->nullable()->comment('户籍所在地');
+            $table->string('graduated_school',100)->nullable()->comment('毕业院校');
+            $table->string('address',100)->nullable()->comment('住宅地址');
+            $table->char('sex')->nullable()->comment("性别");
+            $table->string('phone')->nullable()->comment("手机");
+            $table->string('qq')->nullable()->comment("qq");
+            //$table->string('entry_date')->nullable()->comment('入职日期');
+            $table->text('remark')->nullable()->comment("备注");
+
             $table->rememberToken();
             $table->timestamps();
         });
